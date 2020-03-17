8759453d6d753aec135baea8731f236c88f72db6
Maria Mihai <maria.ic.mihai@gmail.com>
2020-03-17 18:10:31 +0100

    Section 2 - Lets build an API: - adding Student model and controller for it - adding getStudent method based on student Id

---
+++ src/main/java/com/amigoscode/spring/security/student/Student.java
@@ -0,0 +1,22 @@
+package com.amigoscode.spring.security.student;
+
+public class Student {
+
+    private final Integer studentId;
+    private final String name;
+
+
+    public Student(Integer studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+    }
+
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
