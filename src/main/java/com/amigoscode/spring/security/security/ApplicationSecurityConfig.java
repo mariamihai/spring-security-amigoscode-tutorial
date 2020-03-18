134c184be741a738c4bf3e09d0fa5307c50489f0
Maria Mihai <maria.ic.mihai@gmail.com>
2020-03-18 11:25:27 +0100

    Section 3 - In Memory User Details Manager - defining a new user by overriding the userDetailsService() method - which is how you retrieve the users from the db

--- src/main/java/com/amigoscode/spring/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigoscode/spring/security/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.amigoscode.spring.security.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +25,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic(); // Basic Auth to be used
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+//        return super.userDetailsService();
+        UserDetails userDetails = User.builder()
+                    .username("student1")
+                    .password("pass") // Not encoded, will not work yet
+                    .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
