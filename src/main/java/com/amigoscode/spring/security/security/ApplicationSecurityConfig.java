f0378c6b83f90372a61c73705ef32adcef47252c
Maria Mihai <maria.ic.mihai@gmail.com>
2020-03-18 17:50:49 +0100

    Section 4 - hasAuthority() - adding antMatches for the requests defined in the StudentManagementController

--- src/main/java/com/amigoscode/spring/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigoscode/spring/security/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.amigoscode.spring.security.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import static com.amigoscode.spring.security.security.ApplicationUserRole.*;
+import static com.amigoscode.spring.security.security.ApplicationUserAuthority.*;
 
 @Configuration
 @EnableWebSecurity
@@ -32,6 +34,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
