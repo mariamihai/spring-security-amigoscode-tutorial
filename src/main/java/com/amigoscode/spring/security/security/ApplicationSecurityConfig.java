fd5296b00e635a15db396ab44289e3987b9519a3
Maria Mihai <maria.ic.mihai@gmail.com>
2020-03-18 17:37:35 +0100

    Section 4 - Disabling CSRF - disabled csrf - to be updated in the next section - added the toString() method for the Student class

--- src/main/java/com/amigoscode/spring/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigoscode/spring/security/security/ApplicationSecurityConfig.java
@@ -28,6 +28,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
