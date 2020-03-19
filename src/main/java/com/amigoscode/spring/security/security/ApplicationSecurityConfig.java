73149aadee93932d82c34eed2ccc13cbd5f1c26f
Maria Mihai <maria.ic.mihai@gmail.com>
2020-03-19 14:20:09 +0100

    Section 5 - Lets disable CSRF again - disabling CSRF again as it's not needed for this tutorial

--- src/main/java/com/amigoscode/spring/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigoscode/spring/security/security/ApplicationSecurityConfig.java
@@ -31,8 +31,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .and()
+                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
