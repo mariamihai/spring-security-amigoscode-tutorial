271d298dd2609c827aa90a397447a0bbbae47042
Maria Mihai <maria.ic.mihai@gmail.com>
2020-03-23 14:33:32 +0100

    Section 8 - JWT Token Verifier Filter - added filter to check if the token is valid or not - obtaining the claims should be done in a different ways (deprecated method)

---
+++ src/main/java/com/amigoscode/spring/security/jwt/JwtTokenVerifier.java
@@ -0,0 +1,65 @@
+package com.amigoscode.spring.security.jwt;
+
+import com.google.common.base.Strings;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class JwtTokenVerifier extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        // Reject the request as it cannot be authenticated
+        if(Strings.isNullOrEmpty(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            String token = authorizationHeader.replace("Bearer ", "");
+            String key = "keykeykeykeykeykeykeykeykeykeykeykeykeykeykeykey";
+
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .setSigningKey(Keys.hmacShaKeyFor(key.getBytes()))
+                    .parseClaimsJws(token);
+
+            Claims body = claimsJws.getBody();
+            String username = body.getSubject();
+
+            var authorities = (List<Map<String, String>>) body.get("authorities");
+            Set<SimpleGrantedAuthority> authoritiesSet = authorities.stream()
+                    .map(m -> new SimpleGrantedAuthority(m.get("authority")))
+                    .collect(Collectors.toSet());
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    authoritiesSet);
+            // The client that sent the token is now authenticated
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (JwtException e) {
+            throw new IllegalStateException("Token cannot be trusted.");
+        }
+    }
+}
