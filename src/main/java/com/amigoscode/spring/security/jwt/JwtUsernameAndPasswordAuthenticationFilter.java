cc6fbb088c78709d20766df147393e00437dbf18
Maria Mihai <maria.ic.mihai@gmail.com>
2020-03-24 13:12:07 +0100

    Section 8 - JWT Config in Action - refactored code - using tokenExpirationAfterDays property instead of hardcoded value

--- src/main/java/com/amigoscode/spring/security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/amigoscode/spring/security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -57,7 +57,7 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                 .setSubject(authResult.getName())
                 .claim("authorities", authResult.getAuthorities()) // the body
                 .setIssuedAt(new Date())
-                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays())))
                 .signWith(secretKey)
                 .compact();
 
